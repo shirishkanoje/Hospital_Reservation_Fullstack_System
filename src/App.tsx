668705c93add672b3271cfa00dca7cc2b4160780
Shirish Kanoje <152640237+shirishkanoje@users.noreply.github.com>
2025-09-04 14:03:32 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Preloader from "./assets/components/Preloader";
 import Navbar from "./assets/components/Navbar";
